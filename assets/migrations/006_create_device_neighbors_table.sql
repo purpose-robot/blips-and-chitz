5af7cec18adfb73089692020d94879ffbc7cd395
Stefan Media <stefan.media@mailbox.org>
2026-08-27 19:58:29 +0000

    refactor: Format SQL statements

--- assets/migrations/006_create_device_neighbors_table.sql
+++ assets/migrations/006_create_device_neighbors_table.sql
@@ -1,14 +1,14 @@
 -- +goose Up
 CREATE TABLE device_neighbors (
-    id           uuid    PRIMARY KEY DEFAULT gen_random_uuid(),
-    device_id    uuid    NOT NULL REFERENCES devices (id) ON DELETE CASCADE,
-    local_port   text    NOT NULL,
-    remote_port  text    NOT NULL,
-    capabilities text[]  NOT NULL DEFAULT '{}',
+    id           uuid     PRIMARY KEY DEFAULT gen_random_uuid(),
+    device_id    uuid     NOT NULL REFERENCES devices (id) ON DELETE CASCADE,
+    local_port   text     NOT NULL,
+    remote_port  text     NOT NULL,
     mac_address  macaddr,
-    model        text    NOT NULL DEFAULT '',
-    hostname     text    NOT NULL DEFAULT '',
+    model        text     NOT NULL DEFAULT '',
+    hostname     text     NOT NULL DEFAULT '',
     ip_address   inet,
+    capabilities text[]   NOT NULL DEFAULT '{}',
 
     UNIQUE (device_id, local_port, remote_port)
 );

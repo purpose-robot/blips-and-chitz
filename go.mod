cb9083213ce59577fecf65fbae6567a743a374c1
Stefan Media <stefan.media@mailbox.org>
2026-08-27 14:41:16 +0000

    feat: Create internal scrapli package

--- go.mod
+++ go.mod
@@ -7,17 +7,20 @@ require (
 	github.com/jackc/pgerrcode v0.0.0-20250907135507-afb5586c32a6
 	github.com/jackc/pgx/v5 v5.10.0
 	github.com/riverqueue/river v0.45.0
+	github.com/scrapli/scrapligo/v2 v2.0.0-rc.18
 	github.com/wneessen/go-mail v0.8.1
 	golang.org/x/time v0.15.0
 )
 
 require (
+	github.com/ebitengine/purego v0.10.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/riverqueue/river/riverdriver v0.45.0 // indirect
 	github.com/riverqueue/river/rivershared v0.45.0 // indirect
 	github.com/riverqueue/river/rivertype v0.45.0 // indirect
+	github.com/sirikothe/gotextfsm v1.1.0 // indirect
 	github.com/tidwall/gjson v1.19.0 // indirect
 	github.com/tidwall/match v1.2.0 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
---
+++ internal/scrapli/netconf.go
@@ -0,0 +1,72 @@
+package scrapli
+
+import (
+	"context"
+	"encoding/xml"
+	"fmt"
+	"strings"
+
+	scrapligonetconf "github.com/scrapli/scrapligo/v2/netconf"
+	scrapligooptions "github.com/scrapli/scrapligo/v2/options"
+)
+
+type Session struct {
+	host   string
+	driver *scrapligonetconf.Netconf
+}
+
+func (s *Session) Get(ctx context.Context, filter string, reply any) error {
+	results, err := s.driver.Get(ctx, scrapligonetconf.WithFilter(filter))
+	if err != nil {
+		return fmt.Errorf("scrapli.netconf.get: device %s: %v", s.host, err)
+	}
+
+	if results.Failed {
+		return fmt.Errorf("scrapli.netconf.get: device %s rejected rpc: %s", s.host, strings.Join(results.Errors, "; "))
+	}
+
+	err = xml.Unmarshal([]byte(results.Result), reply)
+	if err != nil {
+		return fmt.Errorf("scrapli.netconf.get: device %s: parsing reply: %v", s.host, err)
+	}
+
+	return nil
+}
+
+func (s *Session) Close(ctx context.Context) error {
+	results, err := s.driver.Close(ctx)
+	if err != nil {
+		return fmt.Errorf("scrapli.netconf.close: device %s: %v", s.host, err)
+	}
+
+	if results.Failed {
+		return fmt.Errorf("scrapli.netconf.close: device %s rejected rpc: %s", s.host, strings.Join(results.Errors, "; "))
+	}
+
+	return nil
+}
+
+func Dial(ctx context.Context, host, username, password string) (*Session, error) {
+	driver, err := scrapligonetconf.NewNetconf(
+		host,
+		scrapligooptions.WithUsername(username),
+		scrapligooptions.WithPassword(password),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("scrapli.netconf.dial: device %s: %v", host, err)
+	}
+
+	session := &Session{host: host, driver: driver}
+
+	results, err := driver.Open(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("scrapli.netconf.dial: device %s: %v", host, err)
+	}
+
+	if results.Failed {
+		_ = session.Close(context.WithoutCancel(ctx))
+		return nil, fmt.Errorf("scrapli.netconf.dial: device %s rejected session: %s", host, strings.Join(results.Errors, "; "))
+	}
+
+	return session, nil
+}

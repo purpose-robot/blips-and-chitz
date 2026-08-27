d5b059c0fdccf2c07290ce5a897dbdf1b64dc6d9
Stefan Media <stefan.media@mailbox.org>
2026-08-27 19:45:59 +0000

    feat: Add cisco gateway

---
+++ external/cisco/facts.go
@@ -0,0 +1,161 @@
+package cisco
+
+import (
+	"net/netip"
+	"strings"
+
+	"github.com/purpose-robot/blips-and-chitz/inventory"
+)
+
+const filterFacts = `
+	<native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+		<version/>
+		<hostname/>
+	</native>
+	<device-hardware-data xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-device-hardware-oper">
+		<device-hardware>
+			<device-inventory/>
+		</device-hardware>
+	</device-hardware-data>
+	<stack-oper-data xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-stack-oper"/>
+	<cdp-neighbor-details xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-cdp-oper"/>`
+
+const (
+	roleActive    = "role-active"
+	roleStandby   = "role-standby"
+	hwTypeChassis = "hw-type-chassis"
+)
+
+type factsReply struct {
+	Hostname    string            `xml:"data>native>hostname"`
+	MAC         string            `xml:"data>stack-oper-data>stack-info>stack-mac-address"`
+	Neighbors   []neighborDetail  `xml:"data>cdp-neighbor-details>cdp-neighbor-detail"`
+	SWVersion   string            `xml:"data>native>version"`
+	StackNodes  []stackNode       `xml:"data>stack-oper-data>stack-node"`
+	Inventories []deviceInventory `xml:"data>device-hardware-data>device-hardware>device-inventory"`
+}
+
+func (r *factsReply) parse() *inventory.Facts {
+	facts := &inventory.Facts{
+		Hostname:  r.Hostname,
+		SWVersion: r.SWVersion,
+	}
+
+	if mac, err := inventory.ParseMAC(r.MAC); err == nil {
+		facts.MAC = mac
+	}
+
+	for _, detail := range r.Neighbors {
+		facts.Neighbors = append(facts.Neighbors, detail.parse())
+	}
+
+	models := r.chassisModels()
+
+	for _, node := range r.StackNodes {
+		facts.StackMembers = append(facts.StackMembers, node.parse(models[node.ChassisNumber]))
+	}
+
+	return facts
+}
+
+func (r *factsReply) chassisModels() map[int]string {
+	models := make(map[int]string, len(r.Inventories))
+
+	for _, item := range r.Inventories {
+		if item.HWType == hwTypeChassis {
+			models[item.HWDevIndex] = item.PartNumber
+		}
+	}
+
+	return models
+}
+
+type deviceInventory struct {
+	HWType     string `xml:"hw-type"`
+	PartNumber string `xml:"part-number"`
+	HWDevIndex int    `xml:"hw-dev-index"`
+}
+
+type stackNode struct {
+	MAC           string `xml:"mac-address"`
+	Role          string `xml:"role"`
+	SerialNumber  string `xml:"serial-number"`
+	ChassisNumber int    `xml:"chassis-number"`
+}
+
+func (n stackNode) parse(model string) inventory.StackMember {
+	member := inventory.StackMember{
+		Slot:         n.ChassisNumber,
+		Role:         parseMemberRole(n.Role),
+		Model:        model,
+		SerialNumber: n.SerialNumber,
+	}
+
+	if mac, err := inventory.ParseMAC(n.MAC); err == nil {
+		member.MAC = mac
+	}
+
+	return member
+}
+
+func parseMemberRole(role string) inventory.MemberRole {
+	switch role {
+	case roleActive:
+		return inventory.RolePrimary
+
+	case roleStandby:
+		return inventory.RoleStandby
+
+	default:
+		return inventory.RoleMember
+	}
+}
+
+type neighborDetail struct {
+	LocalPort       string `xml:"local-intf-name"`
+	RemotePort      string `xml:"port-id"`
+	IPAddr          string `xml:"ip-address"`
+	DeviceName      string `xml:"device-name"`
+	Capabilities    string `xml:"capability"`
+	Platform        string `xml:"platform-name"`
+	NeighborPortMAC string `xml:"neighbor-port-mac"`
+}
+
+func (n neighborDetail) parse() inventory.Neighbor {
+	neighbor := inventory.Neighbor{
+		LocalPort:    n.LocalPort,
+		RemotePort:   n.RemotePort,
+		Model:        n.Platform,
+		Hostname:     n.DeviceName,
+		Capabilities: parseCapabilities(n.Capabilities),
+	}
+
+	if ip, err := netip.ParseAddr(n.IPAddr); err == nil {
+		neighbor.IPAddress = ip
+	}
+
+	if mac, err := inventory.ParseMAC(n.NeighborPortMAC); err == nil {
+		neighbor.MAC = mac
+	}
+
+	return neighbor
+}
+
+func parseCapabilities(names string) []inventory.Capability {
+	var capabilities []inventory.Capability
+
+	for name := range strings.FieldsSeq(names) {
+		switch name {
+		case "Switch":
+			capabilities = append(capabilities, inventory.CapabilitySwitch)
+
+		case "Router":
+			capabilities = append(capabilities, inventory.CapabilityRouter)
+
+		case "Trans-Bridge":
+			capabilities = append(capabilities, inventory.CapabilityAccessPoint)
+		}
+	}
+
+	return capabilities
+}
---
+++ external/cisco/gateway.go
@@ -0,0 +1,42 @@
+package cisco
+
+import (
+	"context"
+	"fmt"
+	"net/netip"
+
+	"github.com/purpose-robot/blips-and-chitz/internal/netconf"
+	"github.com/purpose-robot/blips-and-chitz/inventory"
+)
+
+type DeviceGateway struct {
+	username string
+	password string
+}
+
+func NewDeviceGateway(username, password string) *DeviceGateway {
+	return &DeviceGateway{
+		username: username,
+		password: password,
+	}
+}
+
+func (d *DeviceGateway) GatherFacts(ctx context.Context, ipAddress netip.Addr) (*inventory.Facts, error) {
+	session, err := netconf.Dial(ctx, ipAddress.String(), d.username, d.password)
+	if err != nil {
+		return nil, fmt.Errorf("ext.cisco.gatherFacts: %w", err)
+	}
+
+	defer func() {
+		_ = session.Close(context.WithoutCancel(ctx))
+	}()
+
+	reply := new(factsReply)
+
+	err = session.Get(ctx, filterFacts, reply)
+	if err != nil {
+		return nil, fmt.Errorf("ext.cisco.gatherFacts: %w", err)
+	}
+
+	return reply.parse(), nil
+}

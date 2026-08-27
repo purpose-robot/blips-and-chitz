43af641c3ad6cc34f66ff70aff19f807595f8b1c
Stefan Media <stefan.media@mailbox.org>
2026-08-27 16:58:50 +0000

    refactor: Rename package

--- internal/netconf/netconf.go
+++ internal/netconf/netconf.go
@@ -1,4 +1,4 @@
-package scrapli
+package netconf
 
 import (
 	"context"
@@ -16,18 +16,22 @@ type Session struct {
 }
 
 func (s *Session) Get(ctx context.Context, filter string, reply any) error {
-	results, err := s.driver.Get(ctx, scrapligonetconf.WithFilter(filter))
+	opts := []scrapligonetconf.Option{
+		scrapligonetconf.WithFilter(filter),
+	}
+
+	results, err := s.driver.Get(ctx, opts...)
 	if err != nil {
-		return fmt.Errorf("scrapli.netconf.get: device %s: %v", s.host, err)
+		return fmt.Errorf("netconf.get: device %s: %v", s.host, err)
 	}
 
 	if results.Failed {
-		return fmt.Errorf("scrapli.netconf.get: device %s rejected rpc: %s", s.host, strings.Join(results.Errors, "; "))
+		return fmt.Errorf("netconf.get: device %s rejected rpc: %s", s.host, strings.Join(results.Errors, "; "))
 	}
 
 	err = xml.Unmarshal([]byte(results.Result), reply)
 	if err != nil {
-		return fmt.Errorf("scrapli.netconf.get: device %s: parsing reply: %v", s.host, err)
+		return fmt.Errorf("netconf.get: device %s: parsing reply: %v", s.host, err)
 	}
 
 	return nil
@@ -36,11 +40,11 @@ func (s *Session) Get(ctx context.Context, filter string, reply any) error {
 func (s *Session) Close(ctx context.Context) error {
 	results, err := s.driver.Close(ctx)
 	if err != nil {
-		return fmt.Errorf("scrapli.netconf.close: device %s: %v", s.host, err)
+		return fmt.Errorf("netconf.close: device %s: %v", s.host, err)
 	}
 
 	if results.Failed {
-		return fmt.Errorf("scrapli.netconf.close: device %s rejected rpc: %s", s.host, strings.Join(results.Errors, "; "))
+		return fmt.Errorf("netconf.close: device %s rejected rpc: %s", s.host, strings.Join(results.Errors, "; "))
 	}
 
 	return nil
@@ -53,19 +57,19 @@ func Dial(ctx context.Context, host, username, password string) (*Session, error
 		scrapligooptions.WithPassword(password),
 	)
 	if err != nil {
-		return nil, fmt.Errorf("scrapli.netconf.dial: device %s: %v", host, err)
+		return nil, fmt.Errorf("netconf.dial: device %s: %v", host, err)
 	}
 
 	session := &Session{host: host, driver: driver}
 
 	results, err := driver.Open(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("scrapli.netconf.dial: device %s: %v", host, err)
+		return nil, fmt.Errorf("netconf.dial: device %s: %v", host, err)
 	}
 
 	if results.Failed {
 		_ = session.Close(context.WithoutCancel(ctx))
-		return nil, fmt.Errorf("scrapli.netconf.dial: device %s rejected session: %s", host, strings.Join(results.Errors, "; "))
+		return nil, fmt.Errorf("netconf.dial: device %s rejected session: %s", host, strings.Join(results.Errors, "; "))
 	}
 
 	return session, nil
